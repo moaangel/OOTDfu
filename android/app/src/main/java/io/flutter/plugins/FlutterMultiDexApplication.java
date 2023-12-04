b6cb87c857fb3755e1aec9fcbd715af586ab36d2
moaangel <016vhsdkdl@naver.com>
2023-12-04 19:53:47 +0900

    =data 수정

---
+++ android/app/src/main/java/io/flutter/plugins/FlutterMultiDexApplication.java
@@ -0,0 +1,8 @@
+package io.flutter.plugins;
+
+import androidx.multidex.MultiDexApplication;
+
+public class FlutterMultiDexApplication extends MultiDexApplication {
+    // 필요한 경우 Application 클래스에 커스텀 로직을 추가할 수 있습니다.
+    // 별도의 로직이 없다면 이 파일만 생성하고 내용을 비워두셔도 괜찮습니다.
+}
